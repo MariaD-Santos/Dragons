1fb4eda760fdcbbf6a0904e1a8a6d0712400816a
MariaD-Santos <maria.j.santos108@aluno.senai.br>
2026-02-12 17:15:35 -0300

    refeito

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
+import mysql.connector
 
 app = Flask(__name__)
+
+@app.route("/")
+
+@app.route("/home", methods=["GET"])
+def pagina_principal():
+
+    # conectando no BCD
+    conexao = mysql.connector.connect(
+        host="localhost",
+        port = 3306,
+        user = "root",
+        password = "root",
+        database = "MusicTube"
+    )
+
+    # criando o cursos
+    cursor = conexao.cursor(dictionary=True)  # entrega os valores das colunas
+
+    # executando a consulta
+
+    return render_template("principal.html")
+
+@app.route("/admin")
+def pagina_adm():
+    return render_template("administracao.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- projeto/diagrama_music.sql
+++ projeto/diagrama_music.sql
@@ -4,7 +4,6 @@ use MusicTube;
 
 CREATE TABLE IF NOT EXISTS genero (
  nome VARCHAR(30) NOT NULL PRIMARY KEY,
- descricao VARCHAR(100),
  icone CHAR(100),
  cor CHAR(10)
 );
@@ -13,12 +12,9 @@ CREATE TABLE IF NOT EXISTS genero (
 CREATE TABLE IF NOT EXISTS musica (
  codigo INT NOT NULL PRIMARY KEY auto_increment,
  cantor VARCHAR(50),
- genero VARCHAR(30),
  duracao TIME,
  nome VARCHAR(50),
  url_imagem VARCHAR(255),
  nome_genero VARCHAR(30),
  CONSTRAINT fk_musica_genero FOREIGN KEY (nome_genero) REFERENCES genero (nome)
 );
-
-
