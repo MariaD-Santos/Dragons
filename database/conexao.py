f7cd5b2498e4c26d32981537edff886e895aac30
MariaD-Santos <maria.j.santos108@aluno.senai.br>
2026-03-27 09:52:39 -0300

    ADD:trocado o nome da pasta

--- database/conexao.py
+++ database/conexao.py
@@ -4,13 +4,23 @@ import mysql.connector
 
 
 def conectar():
-        conexao = mysql.connector.connect(
-                host="localhost",
-                port = 3306,
-                user = "root",
-                password = "root",
-                database = "MusicTube"
-            )
+        tipo_conexao = "NUVEM"
+        if tipo_conexao == "LOCAL":
+            conexao = mysql.connector.connect(
+                    host="localhost",
+                    port = 3306,
+                    user = "root",
+                    password = "root",
+                    database = "MusicTube"
+                )
+        else:
+            conexao = mysql.connector.connect(
+                    host="servidor-majwaw-server-welcome.a.aivencloud.com",
+                    port = 11569,
+                    user = "avnadmin",
+                    password = "AVNS_1evNwUdL6W4JTZJ5MEM",
+                    database = "MusicTube"
+                )
 
             # criando o cursos
         cursor = conexao.cursor(dictionary=True)  # entrega os valores das colunas
